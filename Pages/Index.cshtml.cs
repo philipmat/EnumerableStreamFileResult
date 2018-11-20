a928ef270796c5085fdd9c3e478748860e83dbb2
Philip Mateescu <github@philipmat.net>
2018-11-19 23:31:55 -0600

    example writing an enumeration to output

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,7 +19,35 @@ namespace EnumerableStreamFileResult.Pages
 
         public IActionResult OnPostDownload(int recordCount)
         {
-            return Content($"Will download {recordCount}.");
+            var lines = Enumerable
+                .Range(1, recordCount)
+                .Select(i => new LineNumber(i));
+
+            return new EnumerableFileStreamer<LineNumber>(lines, "application/vnd.ms-excel") {
+                FileDownloadName = $"Lines {recordCount}.csv"
+            };
+        }
+
+        private class LineNumber : IStreamWriteable
+        {
+            public LineNumber(int lineNumber)
+                => Position = lineNumber;
+
+            public int Position { get; }
+
+            public async Task WriteAsync(Stream stream)
+            {
+                byte[] line = Encoding.UTF8.GetBytes($"{Position},Line-{Position:n0}{Environment.NewLine}");
+                await stream.WriteAsync(line, 0, line.Length);
+            }
+
+            public Task WriteFooterAsync(Stream stream) => Task.CompletedTask;
+
+            public async Task WriteHeaderAsync(Stream stream)
+            {
+                byte[] line = Encoding.UTF8.GetBytes($"Line Number, Line Text{Environment.NewLine}");
+                await stream.WriteAsync(line, 0, line.Length);
+            }
         }
     }
 }
